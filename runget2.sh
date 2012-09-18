9c4663ddf09d1fca2c682292175786199187755c
James Michael DuPont <jamesmikedupont@googlemail.com>
2012-09-18 15:01:30 +0200

    more scripts

---
+++ runget2.sh
@@ -0,0 +1,1 @@
+./osmupdate -v --minutely --max-days=15551 --keep-tempfiles --planet-url=http://pine02.fosm.org/planet/  2011-07-15T23:30:00Z osm.osm 
---
+++ rungetcommonmap.sh
@@ -0,0 +1,1 @@
+../osmupdate -v --hourly --max-days=15551 --keep-tempfiles --planet-url=http://planet.development.i386.commonmap.org/  2011-07-15T23:30:00Z osm.osm 
